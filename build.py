8d3a59086f714d6bbced69e8e77ea2a83c42d4dd
wyf9 <wyf9@wyf9.top>
2024-07-26 21:24:44 +0800

    yfq/poem: add poem day8

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 # from .note.cpp.refresh import Main as refresh
 from datetime import datetime
-from os import environ as env
+import os
 
 
 def info(log):
@@ -47,8 +47,28 @@ try:
 except:
     err("get CF Pages env failed, will not replace index.html.")
 
+# ---
 
-# ----------
-# call note/cpp/Refresh.py
 
-# refresh() # blocked
+# def get_nested_subdirectory_paths(directory, prefix=''):
+#     subdirectories = [os.path.join(directory, d) for d in os.listdir(directory)
+#                       if os.path.isdir(os.path.join(directory, d)) and d not in exclude]
+
+#     # 递归调用，获取每一个子目录下的所有子目录
+#     for subdir in subdirectories:
+#         yield from get_nested_subdirectory_paths(subdir, prefix + subdir + '/' if prefix else subdir)
+
+#     # 如果不在递归中，返回目录本身
+#     if prefix:
+#         yield prefix
+
+
+# exclude = ['.git', '__pycache__', '__MACOSX', 'src']  # 添加你需要排除的目录
+
+# # 获取所有子目录（包括多层）
+# nested_paths = list(get_nested_subdirectory_paths('./'))
+# all_paths = []
+# for n in nested_paths:
+#     all_paths += [n.replace('./', '')]
+
+# print(all_paths)
